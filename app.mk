7c0a20203866f325f9ac7bc3f0ae315ca720848b
tx7do <yanglinbo@gmail.com>
2022-11-11 18:19:09 +0800

    feat: 尝试引入buf.build，但是实际应用下来，并不合适。

--- app.mk
+++ app.mk
@@ -14,12 +14,14 @@ init:
 	@go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 	@go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 	@go install github.com/go-kratos/kratos/cmd/protoc-gen-go-http/v2@latest
-	@go install github.com/google/wire/cmd/wire@latest
 	@go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2@latest
+	@go install github.com/envoyproxy/protoc-gen-validate@latest
+	@go install github.com/google/wire/cmd/wire@latest
 	@go install entgo.io/ent/cmd/ent@latest
 	@go install github.com/swaggo/swag/cmd/swag@latest
 	@go install github.com/google/gnostic@latest
 	@go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
+	@go install github.com/bufbuild/buf/cmd/buf@latest
 
 # 下载依赖库
 .PHONY: dep
