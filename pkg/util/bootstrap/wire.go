591696fe069a1e8f2609a3bf6fa273d246f4a5f2
tx7do <yanglinbo@gmail.com>
2022-12-20 20:05:15 +0800

    feat: update wire util

--- pkg/util/bootstrap/wire.go
+++ pkg/util/bootstrap/wire.go
@@ -1,7 +1,9 @@
 package bootstrap
 
-// FIX: missing go.sum entry for module providing package github.com/google/subcommands
+// FIX: missing go.sum entry for module providing package XXXXXXXXXXXXXXXXXXXXX
 
 import (
 	_ "github.com/google/subcommands"
+	_ "golang.org/x/tools/go/ast/astutil"
+	_ "golang.org/x/tools/go/packages"
 )
