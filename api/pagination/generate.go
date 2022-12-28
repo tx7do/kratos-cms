c2a404e06fd45eb637a8ed4dfb8451a1a4f9d163
tx7do <yanglinbo@gmail.com>
2022-12-28 10:32:12 +0800

    feat: refactor.

--- api/pagination/generate.go
+++
@@ -1,7 +0,0 @@
-package pagination
-
-// 生成 proto
-//go:generate protoc -I=. --go_out=paths=source_relative:. ./*.proto
-
-// 生成 openapi doc
-//go:generate protoc -I=. -I=../ --openapiv2_out . --openapiv2_opt logtostderr=true --openapiv2_opt json_names_for_fields=true ./*.proto
