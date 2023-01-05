bacb9563e68bed795bac62ddce0a3c80d842af66
tx7do <yanglinbo@gmail.com>
2023-01-05 14:11:40 +0800

    fix: proto generate bug.

--- api/comment/service/v1/proto/generate.go
+++ api/comment/service/v1/proto/generate.go
@@ -1,16 +1,16 @@
 package v1
 
 // 生成 proto
-//go:generate protoc -I=. -I=../../../../../third_party -I=../../../../pagination -I=../../../../ --go_out=paths=source_relative:../ ./*.proto
+//go:generate protoc -I=. -I=../../../../../third_party -I=../../../../pagination/proto/ -I=../../../../ --go_out=paths=source_relative:../ ./*.proto
 
 // 生成 proto grpc
-//go:generate protoc -I=. -I=../../../../../third_party -I=../../../../pagination -I=../../../../ --go-grpc_out=paths=source_relative:../ ./*.proto
+//go:generate protoc -I=. -I=../../../../../third_party -I=../../../../pagination/proto/ -I=../../../../ --go-grpc_out=paths=source_relative:../ ./*.proto
 
 // 生成 proto http
-//go:generate protoc -I=. -I=../../../../../third_party -I=../../../../pagination -I=../../../../ --go-http_out=paths=source_relative:../ ./*.proto
+//go:generate protoc -I=. -I=../../../../../third_party -I=../../../../pagination/proto/ -I=../../../../ --go-http_out=paths=source_relative:../ ./*.proto
 
 // 生成 proto errors
-//go:generate protoc -I=. -I=../../../../../third_party -I=../../../../pagination -I=../../../../ --go-errors_out=paths=source_relative:../ ./*.proto
+//go:generate protoc -I=. -I=../../../../../third_party -I=../../../../pagination/proto/ -I=../../../../ --go-errors_out=paths=source_relative:../ ./*.proto
 
 // 生成 openapi doc
-//go:generate protoc -I=. -I=../../../../../third_party -I=../../../../pagination -I=../../../../ --openapiv2_out ../ --openapiv2_opt logtostderr=true --openapiv2_opt json_names_for_fields=true ./*.proto
+//go:generate protoc -I=. -I=../../../../../third_party -I=../../../../pagination/proto/ -I=../../../../ --openapiv2_out ../ --openapiv2_opt logtostderr=true --openapiv2_opt json_names_for_fields=true ./*.proto
