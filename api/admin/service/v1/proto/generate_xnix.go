c955ec60865443b9c585395aa822d419519003a8
tx7do <yanglinbo@gmail.com>
2023-01-07 21:28:40 +0800

    feat: >-<

--- api/admin/service/v1/proto/generate_xnix.go
+++ api/admin/service/v1/proto/generate_xnix.go
@@ -2,15 +2,15 @@
 // +build !windows
 
 // 生成 proto
-//go:generate sh -c "protoc -I=. -I=../../../../../third_party -I=../../../../pagination/proto/ -I=../../../../ -I=../../../../comment/service/v1/proto/ -I=../../../../content/service/v1/proto/ -I=../../../../user/service/v1/proto/ -I=../../../../file/service/v1/proto/ --go_out=paths=source_relative:../ ./*.proto"
+//go:generate sh -c "protoc --proto_path=. --proto_path=../../../../../third_party --proto_path=../../../../pagination/proto/ --proto_path=../../../../ --proto_path=../../../../comment/service/v1/proto/ --proto_path=../../../../content/service/v1/proto/ --proto_path=../../../../user/service/v1/proto/ --proto_path=../../../../file/service/v1/proto/ --go_out=paths=source_relative:../ ./*.proto"
 
 // 生成 proto http
-//go:generate sh -c "protoc -I=. -I=../../../../../third_party -I=../../../../pagination/proto/ -I=../../../../ -I=../../../../comment/service/v1/proto/ -I=../../../../content/service/v1/proto/ -I=../../../../user/service/v1/proto/ -I=../../../../file/service/v1/proto/ --go-http_out=paths=source_relative:../ ./*.proto"
+//go:generate sh -c "protoc --proto_path=. --proto_path=../../../../../third_party --proto_path=../../../../pagination/proto/ --proto_path=../../../../ --proto_path=../../../../comment/service/v1/proto/ --proto_path=../../../../content/service/v1/proto/ --proto_path=../../../../user/service/v1/proto/ --proto_path=../../../../file/service/v1/proto/ --go-http_out=paths=source_relative:../ ./*.proto"
 
 // 生成 proto errors
-//go:generate sh -c "protoc -I=. -I=../../../../../third_party -I=../../../../pagination/proto/ -I=../../../../ -I=../../../../comment/service/v1/proto/ -I=../../../../content/service/v1/proto/ -I=../../../../user/service/v1/proto/ -I=../../../../file/service/v1/proto/ --go-errors_out=paths=source_relative:../ ./*.proto"
+//go:generate sh -c "protoc --proto_path=. --proto_path=../../../../../third_party --proto_path=../../../../pagination/proto/ --proto_path=../../../../ --proto_path=../../../../comment/service/v1/proto/ --proto_path=../../../../content/service/v1/proto/ --proto_path=../../../../user/service/v1/proto/ --proto_path=../../../../file/service/v1/proto/ --go-errors_out=paths=source_relative:../ ./*.proto"
 
 // 生成 openapi doc
-//go:generate sh -c "protoc -I=. -I=../../../../../third_party -I=../../../../pagination/proto/ -I=../../../../ -I=../../../../comment/service/v1/proto/ -I=../../../../content/service/v1/proto/ -I=../../../../user/service/v1/proto/ -I=../../../../file/service/v1/proto/ --openapiv2_out ../ --openapiv2_opt logtostderr=true --openapiv2_opt json_names_for_fields=true ./*.proto"
+//go:generate sh -c "protoc --proto_path=. --proto_path=../../../../../third_party --proto_path=../../../../pagination/proto/ --proto_path=../../../../ --proto_path=../../../../comment/service/v1/proto/ --proto_path=../../../../content/service/v1/proto/ --proto_path=../../../../user/service/v1/proto/ --proto_path=../../../../file/service/v1/proto/ --openapiv2_out ../ --openapiv2_opt logtostderr=true --openapiv2_opt json_names_for_fields=true ./*.proto"
 
 package v1
--- api/pagination/proto/generate_xnix.go
+++ api/pagination/proto/generate_xnix.go
@@ -2,9 +2,9 @@
 // +build !windows
 
 // 生成 proto
-//go:generate sh -c "protoc -I=. -I=../../../third_party --go_out=paths=source_relative:../ ./*.proto"
+//go:generate sh -c "protoc --proto_path=. --proto_path=../../../third_party --go_out=paths=source_relative:../ ./*.proto"
 
 // 生成 openapi doc
-//go:generate sh -c "protoc -I=. -I=../../../third_party --openapiv2_out ../ ./*.proto --openapiv2_opt logtostderr=true --openapiv2_opt json_names_for_fields=true"
+//go:generate sh -c "protoc --proto_path=. --proto_path=../../../third_party --openapiv2_out ../ ./*.proto --openapiv2_opt logtostderr=true --openapiv2_opt json_names_for_fields=true"
 
 package proto
