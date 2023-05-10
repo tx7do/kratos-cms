9fde31ff9474a15d20ff0b90315478476cf60b40
tx7do <yanglinbo@gmail.com>
2023-05-10 20:17:19 +0800

    feat: refactor.

--- blog-backend/pkg/util/entgo/util.go
+++ blog-backend/pkg/util/entgo/util.go
@@ -12,50 +12,71 @@ import (
 type WhereConditions []func(s *sql.Selector)
 type OrderConditions []func(*sql.Selector)
 
+// QueryCommandToWhereConditions 查询命令转换为选择条件
+func QueryCommandToWhereConditions(queryCmd map[string]string) WhereConditions {
+	if queryCmd == nil {
+		return nil
+	}
+
+	queryStr, ok := queryCmd["query"]
+	if !ok {
+		return nil
+	}
+
+	var queryMap map[string]string
+	err := json.Unmarshal([]byte(queryStr), &queryMap)
+	if err != nil {
+		return nil
+	}
+
+	var whereCond WhereConditions
+	for k, v := range queryMap {
+		key := stringcase.ToSnakeCase(k)
+		value := v
+		whereCond = append(whereCond, func(s *sql.Selector) {
+			s.Where(sql.EQ(s.C(key), value))
+		})
+	}
+
+	return whereCond
+}
+
+// QueryCommandToOrderConditions 查询命令转换为排序条件
+func QueryCommandToOrderConditions(orderByCmd map[string]string) OrderConditions {
+	if orderByCmd == nil {
+		return nil
+	}
+
+	orderByStr, ok := orderByCmd["orderBy"]
+	if !ok {
+		return nil
+	}
+
+	var orderByMap map[string]string
+	err := json.Unmarshal([]byte(orderByStr), &orderByMap)
+	if err != nil {
+		return nil
+	}
+
+	var orderCond OrderConditions
+	for k, v := range orderByMap {
+		key := stringcase.ToSnakeCase(k)
+		switch strings.ToLower(v) {
+		case "desc", "descending", "descend":
+			orderCond = append(orderCond, func(s *sql.Selector) {
+				s.OrderBy(sql.Desc(s.C(key)))
+			})
+		case "asc", "ascending", "ascend":
+			orderCond = append(orderCond, func(s *sql.Selector) {
+				s.OrderBy(sql.Asc(s.C(key)))
+			})
+		}
+	}
+
+	return orderCond
+}
+
 // QueryCommandToSelector 查询命令转换为选择器
 func QueryCommandToSelector(queryCmd, orderByCmd map[string]string) (WhereConditions, OrderConditions) {
-	var whereCond WhereConditions
-	var orderCond OrderConditions
-
-	if queryCmd != nil {
-		queryStr, ok := queryCmd["query"]
-		if ok {
-			var queryMap map[string]string
-			err := json.Unmarshal([]byte(queryStr), &queryMap)
-			if err == nil {
-				for k, v := range queryMap {
-					key := stringcase.ToSnakeCase(k)
-					value := v
-					whereCond = append(whereCond, func(s *sql.Selector) {
-						s.Where(sql.EQ(s.C(key), value))
-					})
-				}
-			}
-		}
-	}
-
-	if orderByCmd != nil {
-		orderByStr, ok := orderByCmd["orderBy"]
-		if ok {
-			var orderByMap map[string]string
-			err := json.Unmarshal([]byte(orderByStr), &orderByMap)
-			if err == nil {
-				for k, v := range orderByMap {
-					key := stringcase.ToSnakeCase(k)
-					switch strings.ToLower(v) {
-					case "desc", "descending", "descend":
-						orderCond = append(orderCond, func(s *sql.Selector) {
-							s.OrderBy(sql.Desc(s.C(key)))
-						})
-					case "asc", "ascending", "ascend":
-						orderCond = append(orderCond, func(s *sql.Selector) {
-							s.OrderBy(sql.Asc(s.C(key)))
-						})
-					}
-				}
-			}
-		}
-	}
-
-	return whereCond, orderCond
+	return QueryCommandToWhereConditions(queryCmd), QueryCommandToOrderConditions(orderByCmd)
 }
